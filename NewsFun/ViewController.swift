ec3a9b12080443aca975e678f47608b9f4d0c3ab
Hisham Abraham <hishamabraham@gmail.com>
2018-05-31 11:14:04 -0400

    Initial Commit

---
+++ NewsFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFun
+//
+//  Created by Hisham Abraham on 5/31/18.
+//  Copyright © 2018 Hisham Abraham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
